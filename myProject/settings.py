ac3ce6dd4855776070ca86bd534b6d6d3eaee3d0
AJ Basilio <arsherjames.basilio@gsfe.tupcavite.edu.ph>
2022-06-30 12:06:31 +0800

    whitenoiuse

--- myProject/settings.py
+++ myProject/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,7 +138,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/Media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'admissionApp/static')
 ]
 
 
