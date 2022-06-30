cd605dceabf5a9ab553f060d72793db7efb9bc52
AJ Basilio <arsherjames.basilio@gsfe.tupcavite.edu.ph>
2022-06-30 11:17:24 +0800

    Update settings.py

--- myProject/settings.py
+++ myProject/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-# from django.conf import settings
+from django.conf import settings
 
-# settings.configure(DEBUG=True)
+settings.configure(DEBUG=True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
