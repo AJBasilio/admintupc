d28b57b2c9b48b59238c0552fe508618ad0bde36
AJ Basilio <arsherjames.basilio@gsfe.tupcavite.edu.ph>
2022-06-30 11:52:31 +0800

    Update settings.py

--- myProject/settings.py
+++ myProject/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/Media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'admissionApp/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')
 ]
 
 
