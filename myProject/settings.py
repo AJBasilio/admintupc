c8815b06e16ad4938a894ab7e35a4f3d35f7860b
AJ Basilio <arsherjames.basilio@gsfe.tupcavite.edu.ph>
2022-06-30 20:00:07 +0800

    Update settings.py

--- myProject/settings.py
+++ myProject/settings.py
@@ -160,7 +160,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [('localhost', 6379)],
+            "hosts": [os.environ['REDIS_URL']],
         },
     },
 }
