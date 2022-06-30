7a83883004e1540958d853f530181024b53418ac
AJ Basilio <arsherjames.basilio@gsfe.tupcavite.edu.ph>
2022-06-30 11:45:52 +0800

    .

--- myProject/asgi.py
+++ myProject/asgi.py
@@ -8,12 +8,16 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myProject.settings')
+django.setup()
+
 import admissionApp.routing
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myProject.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
